56ae1a34eab7ca4de9eb404b19303754eaf3f484
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2021-07-14 10:25:00 +0100

    fixing bugs

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ const nErrorSpan = document.querySelector("#bill-error");
 const pErrorSpan = document.querySelector("#people-error");
 const pContainer = document.querySelector("#pinput");
 const bContainer = document.querySelector("#binput");
+const tipPerPerson = document.querySelector("#tip-amount");
+const totalPerPerson = document.querySelector("#t-amount");
 let bill = 0;
 let people = 0;
-let tipPercent = 0;
+let tipPercent = 10;
 let tipAmountPerPerson = 0;
 let totalAmountPerPerson = 0;
 let billErrorMessage = "";
@@ -21,41 +23,52 @@ for (let a = 0; a < tipsButtons.length; a++) {
     const target = event.target;
     let value = Number(target.dataset.value);
     tipPercent = value;
+    calculate();
+    for (let b = 0; b < tipsButtons.length; b++) {
+      if (tipsButtons[b].classList.contains("active")) {
+        tipsButtons[b].classList.remove("active");
+        break;
+      }
+    }
+    tipsButtons[a].classList.add("active");
   });
 }
 
 // adding event listener to bill input
-// billInput.addEventListener("change", (event) => {
-//   const value = billInput.value;
-//   if (value <= 0) {
-//       billErrorMessage = "can't be zero or less";
-//     return;
-//   }
+billInput.addEventListener("keyup", (event) => {
+  Validate(billInput, nErrorSpan, bContainer);
+});
 
-//   console.log(typeof bill);
-// });
+numberOfPeople.addEventListener("keyup", (event) => {
+  Validate(numberOfPeople, pErrorSpan, pContainer);
+});
+
+function Validate(field, errorField, parentContainer) {
+  const value = Number(field.value);
+  if (!value || value <= 0) {
+    errorField.innerHTML = "can't be less or zero";
+    parentContainer.classList.add(`${!value ? "error" : "valid"}`);
+  } else {
+    errorField.innerHTML = "";
+    parentContainer.classList.remove("error");
+  }
+  calculate();
+}
+
+function calculate() {
+  let billval = Number(billInput.value);
+  let peopleval = Number(numberOfPeople.value);
+  if (!billval || !peopleval || !tipPercent) {
+    tipPerPerson.innerHTML = "$0.00";
+    totalPerPerson.innerHTML = "$0.00";
+    return;
+  }
+  let percentPerhead = (tipPercent / 100) * billval;
+  let amtPerHead = billval / peopleval;
+  tipPerPerson.innerHTML = `$${percentPerhead.toFixed(2)}`;
+  totalPerPerson.innerHTML = `${amtPerHead.toFixed(2)}`;
+}
 
 actionButton.addEventListener("click", (event) => {
   const billValue = Number(billInput.value);
-  const nOfPeople = Number(numberOfPeople.value);
-  console.log(billValue, " - ", nOfPeople);
-  if (!billValue || !nOfPeople) {
-    console.log("testing");
-    nErrorSpan.hidden = !billValue ? false : true;
-    pErrorSpan.hidden = !nOfPeople ? false : true;
-    nErrorSpan.innerHTML = !billValue ? "can't be less or zero" : "";
-    pErrorSpan.innerHTML = !nOfPeople ? "can't be less or zero" : "";
-    bContainer.classList.add(`${!billValue ? "error" : "valid"}`);
-    pContainer.classList.add(`${!nOfPeople ? "error" : "valid"}`);
-    if (billValue) bContainer.classList.remove("error");
-    if (nOfPeople) pContainer.classList.remove("error");
-
-    return;
-  }
-  nErrorSpan.innerHTML = "";
-  nErrorSpan.hidden = true;
-  pErrorSpan.setAttribute("hidden", "true");
-  pErrorSpan.setAttribute("hidden", "true");
-  bContainer.classList.remove("error");
-  pContainer.classList.remove("error");
 });
